c0706b9e425c7441955c113eeb12b83b402a35e2
Jake Huffman <hokiejake1@gmail.com>
2024-01-07 15:07:20 -0500

    Updates

---
+++ precache-manifest.d1b2359359e861993e02200684702593.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9235fe133eb6264d0a942505b4283bb5",
+    "url": "/art-gallery-react-app/index.html"
+  },
+  {
+    "revision": "354aa2bf763bf0105756",
+    "url": "/art-gallery-react-app/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "65dfe7457e96cf2c02dc",
+    "url": "/art-gallery-react-app/static/js/2.e89fdc35.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/art-gallery-react-app/static/js/2.e89fdc35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312439db6c34f5b25d5e",
+    "url": "/art-gallery-react-app/static/js/3.b2a674e1.chunk.js"
+  },
+  {
+    "revision": "354aa2bf763bf0105756",
+    "url": "/art-gallery-react-app/static/js/main.38a0df40.chunk.js"
+  },
+  {
+    "revision": "4cdefa42d2f29ca6adfd",
+    "url": "/art-gallery-react-app/static/js/runtime-main.10f481ea.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/art-gallery-react-app/static/media/logo.06e73328.svg"
+  }
+]);
